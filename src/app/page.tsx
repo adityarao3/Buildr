efa059519e9193094f3ea1bd8aa5f2a1eb0aaeba
Aditya Rao <adityarao9541@gmail.com>
2025-06-27 02:48:15 +0530

    Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const Page=()=>{
+  
+    return(
+      <div className="font-bold text-rose-500">
+        Hello From Aditya
+        <Button>
+          Click Me for /money 
+        </Button>
+      </div>
+    )
+}
+export default Page;
