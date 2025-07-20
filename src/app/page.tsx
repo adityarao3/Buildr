b9ef14125541debc8f8a9e1026ce5a8f87ce23b5
Aditya Rao <adityarao9541@gmail.com>
2025-07-21 04:20:30 +0530

    09:projectids-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,32 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 
 const Page = () => {
+  const router=useRouter();
   const [value,setValue]=useState("");
 const trpc=useTRPC();
-const {data:messages}=useQuery(trpc.messages.getMany.queryOptions());
-const createMessage=useMutation(trpc.messages.create.mutationOptions({
-  onSuccess:()=>{
-    toast.success("Message created")
+const createProject=useMutation(trpc.projects.create.mutationOptions({
+  onError:(error)=>{
+    toast.error(error.message);
+  },
+  onSuccess:(data)=>{
+    router.push(`/projects/${data.id}`);
   }
 }));
   return (
-   <div className="p-4 max-w-7xl mx-auto">
+   <div className="h-screen w-screen flex items-center justify-center">
+    <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center">
     <Input value={value} onChange={(e)=>setValue(e.target.value)}/>
-    <button  disabled={createMessage.isPending} onClick={()=>createMessage.mutate({value:value})} className="p-4 bg-blue-500 flex border-8 
+    <button  disabled={createProject.isPending} onClick={()=>createProject.mutate({value:value})} className="p-4 bg-blue-500 flex border-8 
     rounded-3xl">
-Invoke Background Button
+Submit
     </button>
-    {JSON.stringify(messages,null,2)}
+   </div>
    </div>
   )
 }
