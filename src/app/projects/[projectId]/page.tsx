49f07960d5fa8b0413ca62f27ba3a763e20bf3f0
Aditya Rao <adityarao9541@gmail.com>
2025-07-22 19:53:18 +0530

    10:messages-ui-setup

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,15 +1,33 @@
-interface Props{
-    params:Promise<{
-        projectId:string;
-    }>
+import { ProjectView } from "@/modules/projects/ui/views/project-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
+interface Props {
+    params: {
+        projectId: string;
+    };
 }
 
-const Page= async({params}:Props)=>{
-    const {projectId}=await params;
+const Page = async ({ params }: Props) => {
+    const { projectId } = params;
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(
+        trpc.messages.getMany.queryOptions({
+            projectId,
+        })
+    );
+    void queryClient.prefetchQuery(
+        trpc.projects.getOne.queryOptions({
+            id: projectId,
+        })
+    );
     return (
-        <div>
-            Project id:{projectId}
-        </div>
-    )
-}
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Suspense fallback={<p>Loading...</p>}>
+                <ProjectView projectId={projectId} />
+            </Suspense>
+        </HydrationBoundary>
+    );
+};
 export default Page;
